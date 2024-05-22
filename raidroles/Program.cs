01e327cd77330b843b8d1aeb277cfd6e79e40699
Sam Streed <embersteelstreus@gmail.com>
2024-05-22 02:51:48 -0500

    added interactive form entry mode

--- raidroles/Program.cs
+++ raidroles/Program.cs
@@ -7,6 +7,7 @@ partial class Program {
 
         var formArg = new Argument<string>(
             name: "form",
+            getDefaultValue: () => "",
             description: ""
         );
         root.Add(formArg);
@@ -20,29 +21,54 @@ partial class Program {
         root.AddOption(seedOption);
 
         var readFromFileOption = new Option<bool>(
-            name: "-f",
+            name: "--file",
             description: "Read form data from a path specified in <form>"
         ) {
             Arity = new ArgumentArity(minimumNumberOfValues: 0, maximumNumberOfValues: 0)
         };
+        readFromFileOption.AddAlias("-f");
         root.AddOption(readFromFileOption);
 
-        root.SetHandler(async (formInput, seed, fromFile) => {
-            SetSeed(seed);
+        var interactiveOption = new Option<bool>(
+            name: "--interactive",
+            description: ""
+        ) {
+            Arity = new ArgumentArity(minimumNumberOfValues: 0, maximumNumberOfValues: 0)
+        };
+        interactiveOption.AddAlias("-i");
+        root.AddOption(interactiveOption);
 
+        root.SetHandler(HandleRoot, formArg, seedOption, readFromFileOption, interactiveOption);
+
+        return await root.InvokeAsync(args);
+    }
+
+    static async Task HandleRoot(string formInput, string seed, bool fromFile, bool interactive) {
+        SetSeed(seed);
+
+        Form? form = null;
+
+        if (interactive) {
+            if (formInput != "" || fromFile) {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.Error.WriteLine("Do not specify form options in interactive mode");
+                Environment.Exit(1);
+            }
+
+            form = HandleInteractiveForm();
+        } else {
             if (fromFile) {
                 var file = new FileInfo(formInput);
                 using var stream = new StreamReader(file.OpenRead());
                 formInput = await stream.ReadToEndAsync();
             }
             
-            var form = JsonConvert.DeserializeObject<Form>(formInput);
-            if (form != null) {
-                var applicants = CollectApplicants(form);
-                SelectRoles(applicants);
-            }
-        }, formArg, seedOption, readFromFileOption);
+            form = JsonConvert.DeserializeObject<Form>(formInput);
+        }
 
-        return await root.InvokeAsync(args);
+        if (form != null) {
+            var applicants = CollectApplicants(form);
+            SelectRoles(applicants);
+        }
     }
 }
